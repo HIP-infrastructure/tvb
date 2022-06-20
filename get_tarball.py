245ceef51a6879b8cbea500e2bf110293232eb9c
Nathalie Casati <nathalie.casati@chuv.ch>
2022-06-20 15:04:26 +0000

    download is now cscs-proof

--- get_tarball.py
+++ get_tarball.py
@@ -6,4 +6,4 @@ repo = g.get_repo('ins-amu/hip-tvb-app')
 rls = repo.get_latest_release()
 ast = rls.get_assets()
 for a in ast:
-    os.system(f'curl -LO# {a.browser_download_url}')
+    os.system(f'curl -LO# --retry 5 --retry 10 --retry-max-time 0 -C - {a.browser_download_url}')
