9975a7f19d19aff034e68d03bf88173ab353ca81
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2022-11-28 16:18:43 +0100

    setup stages in build

--- test.dockerfile
+++ test.dockerfile
@@ -1,5 +1,4 @@
-FROM hip-deploy
+ARG fromimage
+FROM ${fromimage}
 RUN adduser hip
-
-
 USER hip
--- test_dockerfile.sh
+++ test_dockerfile.sh
@@ -7,25 +7,55 @@
 
 set -eux
 
-ub=ubuntu:20.04
-docker pull $ub
-docker tag $ub deploy-hip/nc-webdav:latest
-docker build -t hip-deploy \
-	--build-arg CI_REGISTRY_IMAGE=deploy-hip \
-	--build-arg DAVFS2_VERSION=latest \
-	--build-arg CARD=foo \
-	--build-arg CI_REGISTRY=bar \
-	--build-arg TAG='' \
-	--build-arg APP_NAME=tvb \
-	--build-arg APP_VERSION=0.6 \
-	--build-arg DOCKERFS_TYPE=nc-webdav \
-	--build-arg DOCKERFS_VERSION=latest \
-	.
+# some versions & metadata
+base='ubuntu:20.04'
+ci_registry_image=deploy-hip
+davfs2_version=latest
+dockerfs_type=nc-webdav
+dockerfs_version=latest
+card=foo
+ci_registry=bar
+tag=''
+fs_ver=7.2.0
+fsl_ver=6.0.6
+tvb_ver=0.6
 
-	# --build-arg EBRAINS_TOKEN=$EBRAINS_TOKEN \
-	
-# this just adds a hip user
-docker build -t hip-test -f test.dockerfile .
+# build image, hip style
+function make_hip_image() {
+	app_name=$1
+	app_ver=$2
+	app_build_ctx=$3
+	docker build -t ${ci_registry_image}/${app_name}:${app_ver}${tag} \
+		--build-arg CI_REGISTRY_IMAGE=${ci_registry_image} \
+		--build-arg CARD=${card} \
+		--build-arg CI_REGISTRY=${ci_registry} \
+		--build-arg TAG=${tag} \
+		--build-arg APP_NAME=${app_name} \
+		--build-arg APP_VERSION=${app_ver} \
+		--build-arg DOCKERFS_TYPE=${dockerfs_type} \
+		--build-arg DOCKERFS_VERSION=${dockerfs_version} \
+		--build-arg FREESURFER_VERSION=${fs_ver} \
+		--build-arg FSL_VERSION=${fsl_ver} \
+		$app_build_ctx
+}
+
+# get started
+docker pull $base
+docker tag $base ${ci_registry_image}/${dockerfs_type}:${dockerfs_version}${tag}
+
+# shim directories for fs & fsl stages
+mkdir -p ./stage-{fs,fsl}/scripts
+mkdir -p ./stage-{fs,fsl}/apps/{freesurfer,fsl}/config
+
+# build images
+make_hip_image freesurfer $fs_ver ./stage-fs
+make_hip_image fsl $fsl_ver ./stage-fsl
+make_hip_image tvb $tvb_ver ./
+
+# test image & container
+docker build -t hip-test \
+	--build-arg ${ci_registry_image}/tvb:${tvb_ver}${tag} \
+	-f test.dockerfile .
 
 # start a container for testing
 docker rm -f hip-test || true
